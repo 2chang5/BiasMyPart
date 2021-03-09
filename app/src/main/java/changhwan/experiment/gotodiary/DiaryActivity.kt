2d8677f2caca20d78df08352bf063969c9262712
leeChangHwan <chang5hustle@gmail.com>
2021-03-09 17:31:49 +0900

    keyboard bar but blink

--- app/src/main/java/changhwan/experiment/gotodiary/DiaryActivity.kt
+++ app/src/main/java/changhwan/experiment/gotodiary/DiaryActivity.kt
@@ -1,17 +1,47 @@
 package changhwan.experiment.gotodiary
 
 import android.content.Intent
-import android.graphics.Color
-import android.graphics.Color.parseColor
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_diary.*
 
+
 class DiaryActivity : AppCompatActivity() {
+
+    var softKeyboard: SoftKeyboard? = null
+    var coverlayout: RelativeLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diary)
+
+        //키보드 바 설정용 키보드 올라가고 내려가는 이벤트처리
+        coverlayout = findViewById<View>(R.id.coverlayout) as RelativeLayout
+        val controlManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        softKeyboard = SoftKeyboard(coverlayout, controlManager)
+        softKeyboard!!.setSoftKeyboardCallback(object : SoftKeyboard.SoftKeyboardChanged {
+            override fun onSoftKeyboardHide() {
+                Handler(Looper.getMainLooper()).post {
+                    keyBoardbar.visibility = View.INVISIBLE
+                    forKeyboardSpace.visibility = View.GONE
+                }
+            }
+
+            override fun onSoftKeyboardShow() {
+                Handler(Looper.getMainLooper()).post {
+                    keyBoardbar.visibility = View.VISIBLE
+                    forKeyboardSpace.visibility = View.VISIBLE
+                }
+            }
+        })
+
 
         //x버튼 눌렸을떄 돌아가기
         xButton.setOnClickListener {
@@ -34,7 +64,7 @@ class DiaryActivity : AppCompatActivity() {
 
 
         //배경 카드색깔 젤리에 맞추기
-        val emotion = intent.getIntExtra("emotion",1)
+        val emotion = intent.getIntExtra("emotion", 1)
         if(emotion == 1){
             diaryScrollCard.setBackgroundResource(R.drawable.for_diary_card1)
         }else if(emotion == 2){
@@ -69,5 +99,11 @@ class DiaryActivity : AppCompatActivity() {
 //            val emotion = intent.getIntExtra("emotion",1).toString()
 //            diaryText.text=emotion
 //        }
+    }
+
+    //키보드 관련 디스트로이
+    public override fun onDestroy() {
+        super.onDestroy()
+        softKeyboard!!.unRegisterSoftKeyboardCallback()
     }
 }
